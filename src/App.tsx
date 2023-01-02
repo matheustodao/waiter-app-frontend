57404fa61a987bc1dc95fc794f6a59085935b46f
matheustodao <todaomatheus@hotmail.com>
2023-01-02 13:15:30 -0300

    refactor: import global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 export function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles />
       <h1>Hello World</h1>
-    </div>
+    </>
   );
 }
