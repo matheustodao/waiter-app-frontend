3fa4e0f2fff1009be9ea8a4d211ac86dbcb66594
matheustodao <todaomatheus@hotmail.com>
2023-01-02 14:40:22 -0300

    chore: create order type
    chore: create a order mock

---
+++ src/types/Order.ts
@@ -0,0 +1,16 @@
+type OrderStatus = 'WAITING' | 'IN_PRODUCTION' | 'DONE'
+
+export interface Order {
+  _id: string,
+  table: string,
+  status: OrderStatus,
+  products: {
+    _id: string,
+    quantity: number,
+    product: {
+      name: string,
+      imagePath: string,
+      price: number,
+    }
+  }[]
+}
