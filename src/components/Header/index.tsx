f5da26bfe599e7df8f69169e0a55873fbf2f9c75
matheustodao <todaomatheus@hotmail.com>
2023-01-02 13:43:42 -0300

    chore: create header component
    chore: create header component styles

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/images/logo.svg';
+
+import { Container, Content } from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <div className="page-details">
+          <h1>Pedidos</h1>
+          <h2>Acompanha os pedidos dos clientes</h2>
+        </div>
+
+        <img src={logo} alt="WAITERAPP" />
+      </Content>
+    </Container>
+  );
+}
