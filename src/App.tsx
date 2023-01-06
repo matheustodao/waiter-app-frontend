b39ee8be65805b58a2058a286c61f2400e2b129f
matheustodao <todaomatheus@hotmail.com>
2023-01-06 01:12:24 -0300

    chore: add react toastify
    chore: setup react toastify

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { Header } from './components/Header';
 import { Orders } from './components/Orders';
 import { GlobalStyles } from './styles/GlobalStyles';
 
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
 export function App() {
   return (
     <>
       <GlobalStyles />
       <Header />
       <Orders />
+      <ToastContainer position="bottom-center" />
     </>
   );
 }
